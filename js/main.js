a03fa3426dfde4fee9a6ef62af8cd2292d2f9f47
Akhil Satish <akhilsatish2008@gmail.com>
2022-04-12 23:37:28 -0700

    added logic to add in ingrediants from api and also not add null items to dom

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
-document.querySelector('button').addEventListener('click', getDrink)
+document.querySelector('.search-btn').addEventListener('click', getDrink)
 
 function getDrink(){
     let drink = document.querySelector('input').value
@@ -9,12 +9,24 @@ function getDrink(){
     .then(data => {
       console.log(data.drinks)
       document.querySelector('h2').innerText = data.drinks[0].strDrink
-      document.querySelector('img').src = data.drinks[0].strDrinkThumb
+      document.querySelector('.carousel__img').src = data.drinks[0].strDrinkThumb
       document.querySelector('h3').innerText = data.drinks[0].strInstructions
+
+      let ingredients = Object.keys(data.drinks[0])
+              .filter((key)=> key.includes("strIngredient"))
+              .map((key)=> data.drinks[0][key])
+
+
+      function removeNull(arr){
+        return arr.filter(x=> x!==null)
+      }
+
+      document.querySelector("p").innerText = removeNull(ingredients)
+
+
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
 
 }
-
