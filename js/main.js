330a5c33133dbe19f59cd491935406821d1824a9
Akhil Satish <akhilsatish2008@gmail.com>
2022-04-15 22:48:07 -0700

    changed logic in how fetched dat was being used in local storage

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function getDrink() {
       document.querySelector('h2').innerText = data.drinks[0].strDrink
       document.querySelector('.carousel__img').src = data.drinks[0].strDrinkThumb
       document.querySelector('h3').innerText = data.drinks[0].strInstructions
-      document.querySelector("li").innerText = ingridientsList(0)
+      document.querySelector("p").innerText = ingridientsList(0)
 
 
 
@@ -51,15 +51,11 @@ function getDrink() {
     let list = removeNull(ingredients)
 
     return list;
-  }
+  } 
 
   function nextDrink() {
 
-
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-
+        let data =  JSON.parse(window.localStorage.getItem('data')); 
         let bevList = data.drinks;
         let currentBev = document.querySelector('h2').innerText
         let currBevIndex = 0
@@ -84,7 +80,7 @@ function getDrink() {
           document.querySelector('h2').innerText = bevList[nextBevIndex].strDrink;
           document.querySelector('.carousel__img').src = bevList[nextBevIndex].strDrinkThumb
           document.querySelector('h3').innerText = bevList[nextBevIndex].strInstructions
-          document.querySelector("li").innerText = ingridientsList(nextBevIndex)
+          document.querySelector("p").innerText = ingridientsList(nextBevIndex)
         }
        if (this.id === "prevBtn") {
           if (currBevIndex === 0) {
@@ -98,12 +94,12 @@ function getDrink() {
           document.querySelector('h2').innerText = bevList[nextBevIndex].strDrink;
           document.querySelector('.carousel__img').src = bevList[nextBevIndex].strDrinkThumb
           document.querySelector('h3').innerText = bevList[nextBevIndex].strInstructions
-          document.querySelector("li").innerText = ingridientsList(nextBevIndex)
+          document.querySelector("p").innerText = ingridientsList(nextBevIndex)
 
-        }
-      })
+        
+      
       .catch(err => {
         console.log(`error ${err}`)
-      });
+      })
   }
-}
+}}
